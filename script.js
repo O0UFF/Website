e059f7edabf09d29efe80f6ed2f2d521bd5e1a2c
O0UFF <62802713+O0UFF@users.noreply.github.com>
2023-07-22 00:51:28 +0000

    div visibility with button test 4 (help me)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 {
     document.getElementById("button").onclick = function(){
         document.getElementById("stuff").style.display = "inline";
+        document.getElementById("SillyGooseText").innerHTML = "UWU";
     }
 }
